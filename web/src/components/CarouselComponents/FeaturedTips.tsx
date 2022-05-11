1daa474c16f8aafd066aeef2756c7ddd8926aa0b
Leo-FSardenberg <leo.f.sardenberg@gmail.com>
2022-05-11 10:01:11 -0300

    mudanças de texto

--- web/src/components/CarouselComponents/FeaturedTips.tsx
+++ web/src/components/CarouselComponents/FeaturedTips.tsx
@@ -4,7 +4,7 @@ export const  tips = [
  
 <div className="bg-Orange-200 rounded-full px-3 h-12 flex items-center text-white group">
     <span className="text-xs text-neutral-400 text-black"><a href="https://youtu.be/3pw3iH53e60" className="underline underline-offset-2>">
-  Dicas de autenticação</a></span></div>,
+  Dicas de autenticação,  clicando nesse link você poderá aprender <br></br>com a Rocketseat sobre Phoenix Token</a></span></div>,
 
 <div className="bg-Orange-200 rounded-full px-3 h-12 flex items-center text-white group">
     <span className="text-xs text-neutral-400 text-black"><a href="https://www.youtube.com/watch?v=pFUXbppNpDQ" className="underline underline-offset-2>">
@@ -16,7 +16,7 @@ export const  tips = [
 
 <div className="bg-Orange-200 rounded-full px-3 h-12 flex items-center text-white group">
     <span className="text-xs text-neutral-400 text-black"><a href="https://youtu.be/sHK_9oDiimU" className="underline underline-offset-2>">
-    Dica de animação SVG no React Native</a></span></div>,
+    Dica de animação, Clicando nesse link você poderá aprender <br></br> com a Rocketseat SVG no React Native</a></span></div>,
 ]
 
 
